b31b983fb54ff204159c82d618882f2594ac7597
Austine <chucky891213@naver.com>
2024-07-19 11:50:17 +0900

    24-07-19 event 처리 함수 수정

--- src/step13/ToggleButton2.jsx
+++ src/step13/ToggleButton2.jsx
@@ -4,20 +4,28 @@ export const ToggleButton2 = (props) => {
   const [onAir, setOnAir] = useState(false);
   const [onAir2, setOnAir2] = useState(false);
 
+  // const func = (event, mode) => {
+  //   console.log(mode, "==>", event.target, event.target.textContent);
+  //   if (mode == 1) {
+  //     if (event.target.textContent === "103.5 방송중단") {
+  //       setOnAir(true);
+  //     } else {
+  //       setOnAir(false);
+  //     }
+  //   } else if (mode == 2) {
+  //     if (event.target.textContent === "107.7 방송중단") {
+  //       setOnAir2(true);
+  //     } else {
+  //       setOnAir2(false);
+  //     }
+  //   }
+  // };
+
   const func = (event, mode) => {
-    console.log(mode, "==>", event.target, event.target.textContent);
-    if (mode == 1) {
-      if (event.target.textContent === "103.5 방송중단") {
-        setOnAir(true);
-      } else {
-        setOnAir(false);
-      }
-    } else if (mode == 2) {
-      if (event.target.textContent === "107.7 방송중단") {
-        setOnAir2(true);
-      } else {
-        setOnAir2(false);
-      }
+    if (mode === 1) {
+      setOnAir(!onAir);
+    } else if (mode === 2) {
+      setOnAir2(!onAir2);
     }
   };
 
