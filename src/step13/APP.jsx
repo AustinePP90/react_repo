fa89d39f363b593f7b6d17154303605bfff65e90
Austine <chucky891213@naver.com>
2024-07-19 11:45:23 +0900

    24-07-19 event 처리

---
+++ src/step13/APP.jsx
@@ -0,0 +1,9 @@
+import { ToggleButton2 } from "./ToggleButton2";
+
+export function App() {
+  return (
+    <div>
+      <ToggleButton2 />
+    </div>
+  );
+}
---
+++ src/step13/ToggleButton.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export const ToggleButton = () => {
+  const [onAir, setOnAir] = useState(false);
+  const [onAir2, setOnAir2] = useState(false);
+
+  const func1 = () => {
+    setOnAir(!onAir);
+  };
+
+  const func2 = () => {
+    setOnAir2(!onAir2);
+  };
+  return (
+    <div>
+      <button onClick={func1}>
+        {onAir ? "103.5 방송중" : "103.5 방송중단"}
+      </button>
+      <button onClick={func2}>
+        {onAir2 ? "107.7 방송중" : "107.7 방송중단"}
+      </button>
+    </div>
+  );
+};
